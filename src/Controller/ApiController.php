713aba72fa1b8563805cb15bfb61372f05879c3c
Fred <frederic@pmo.digital>
2020-02-10 05:22:22 +0100

    Etape 13

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ApiController extends AbstractController
+{
+    /**
+     * @Route("/listeRegions", name="listeRegions")
+     */
+    public function listeRegions(SerializerInterface $serializer)
+    {
+        $mesRegions=file_get_contents('https://geo.api.gouv.fr/regions');
+        $mesRegionsTable=$serializer->decode($mesRegions, 'json');
+        $mesRegionsObjet=$serializer->denormalize($mesRegionsTable, 'App\Entity\Region[]');
+        // dump($mesRegionsObjet);
+        // die();
+        return $this->render('api/index.html.twig', [
+           'mesRegions' => $mesRegionsObjet
+        ]);
+    }
+}
