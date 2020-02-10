3187998dc325d82260350ca2f2e02db408d02553
Fred <frederic@pmo.digital>
2020-02-10 07:08:38 +0100

    Etape 18

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,13 +15,41 @@ class ApiController extends AbstractController
     public function listeRegions(SerializerInterface $serializer)
     {
         $mesRegions=file_get_contents('https://geo.api.gouv.fr/regions');
-        // $mesRegionsTable=$serializer->decode($mesRegions, 'json');
-        // $mesRegionsObjet=$serializer->denormalize($mesRegionsTable, 'App\Entity\Region[]');
         $mesRegions=$serializer->deserialize($mesRegions, 'App\Entity\Region[]', 'json');
-        // dump($mesRegionsObjet);
-        // die();
+       
         return $this->render('api/index.html.twig', [
            'mesRegions' => $mesRegions
         ]);
     }
+
+        /**
+     * @Route("/listeDepsParRegion", name="listeDepsParRegion")
+     */
+    public function listeDepsParRegion(Request $request, SerializerInterface $serializer)
+    {
+         // je récupère la région selectionnée dans le formulaire
+         $codeRegion=$request->query->get('region');
+
+        // je récupère les régions
+        $mesRegions=file_get_contents('https://geo.api.gouv.fr/regions');
+        $mesRegions=$serializer->deserialize($mesRegions, 'App\Entity\Region[]', 'json');
+
+       // je récupère les départements
+        if($codeRegion == null || $codeRegion == "Toutes") {
+            $mesDeps=file_get_contents('https://geo.api.gouv.fr/departements');
+        }
+        else {
+            $mesDeps=file_get_contents('https://geo.api.gouv.fr/regions/'.$codeRegion.'/departements');
+        }
+        // décodage du json en tableau
+        $mesDeps=$serializer->decode($mesDeps, 'json');
+       
+        return $this->render('api/listDepsParRegion.html.twig', [
+           'mesRegions' => $mesRegions,
+           'mesDeps' => $mesDeps
+        ]);
+    }
+
+
+
 }
--- src/Entity/Region.php
+++ src/Entity/Region.php
@@ -31,7 +31,7 @@ class Region
 
     public function getCode(): ?string
     {
-        return "n° ".$this->code;
+        return $this->code;
     }
 
     public function setCode(string $code): self
